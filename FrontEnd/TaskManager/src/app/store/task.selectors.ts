b8c9e286f7a70583a281cd666325a876485ac473
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-15 03:56:52 +0300

    Update task.selectors.ts

--- FrontEnd/TaskManager/src/app/store/task.selectors.ts
+++ FrontEnd/TaskManager/src/app/store/task.selectors.ts
@@ -1,6 +1,18 @@
+
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { TaskState } from './task.reducer';
 
-export const selectTaskState = createFeatureSelector<TaskState>('tasks');
-export const selectAllTasks = createSelector(selectTaskState,(state: TaskState) => state.tasks);
 
+export const selectTaskState = createFeatureSelector<TaskState>('tasks');
+
+
+export const selectAllTasks = createSelector(
+  selectTaskState,
+  (state: TaskState) => state.tasks
+);
+
+
+export const selectTaskError = createSelector(
+  selectTaskState,
+  (state: TaskState) => state.error
+);
