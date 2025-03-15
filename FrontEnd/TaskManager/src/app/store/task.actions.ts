9e6544854c1294268d13cfb3e03ed848591733aa
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-15 03:57:19 +0300

    Update task.actions.ts

--- FrontEnd/TaskManager/src/app/store/task.actions.ts
+++ FrontEnd/TaskManager/src/app/store/task.actions.ts
@@ -1,14 +1,27 @@
+
+
 import { createAction, props } from '@ngrx/store';
 
-export interface Task {
-    id: number;
-    task: string;
-    completed?: boolean;  
-  }
-export const addTask = createAction('[Task] Add Task',props<{ task: Task }>());
-export const updateTask = createAction( '[Task] Update Task', props<{ task: Task }>());
-export const deleteTask = createAction('[Task] Delete Task',props<{ id: number }>());
-export const loadTasks = createAction('[Task] Load Tasks');
 
+export interface Task {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+
+
+
+export const loadTasks = createAction('[Task] Load Tasks');
+export const loadTasksSuccess = createAction('[Task] Load Tasks Success', props<{ tasks: Task[] }>());
+export const loadTasksFailure = createAction('[Task] Load Tasks Failure', props<{ error: string }>());
+
+export const addTask = createAction('[Task] Add Task', props<{ task: Task }>());
+
+
+export const updateTask = createAction('[Task] Update Task', props<{ task: Task }>());
+
+
+export const deleteTask = createAction('[Task] Delete Task', props<{ id: number }>());
 
 
