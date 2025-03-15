5e5311e065df5b800168a7f6e35189f3907d9029
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-15 03:56:24 +0300

    Update task.reducer.ts

--- FrontEnd/TaskManager/src/app/store/task.reducer.ts
+++ FrontEnd/TaskManager/src/app/store/task.reducer.ts
@@ -1,19 +1,25 @@
+
+
 import { createReducer, on } from '@ngrx/store';
-import {Task} from './task.actions';
-import { addTask, updateTask, deleteTask, loadTasks } from './task.actions';
+import { Task } from './task.actions';
+import { addTask, updateTask, deleteTask, loadTasks, loadTasksSuccess, loadTasksFailure } from './task.actions';
 
 export interface TaskState {
   tasks: Task[];
+  error: string | null; 
 }
+
 export const initialState: TaskState = {
-  tasks: []
+  tasks: [],
+  error: null
 };
 
 export const taskReducer = createReducer(
   initialState,
+  on(loadTasks, state => ({ ...state, error: null })), 
+  on(loadTasksSuccess, (state, { tasks }) => ({ ...state, tasks, error: null })), 
+  on(loadTasksFailure, (state, { error }) => ({ ...state, error })), 
   on(addTask, (state, { task }) => ({ ...state, tasks: [...state.tasks, task]})),
   on(updateTask, (state, { task }) => ({ ...state, tasks: state.tasks.map(t => (t.id === task.id ? task : t))})),
-  on(deleteTask, (state, { id }) => ({ ...state,tasks: state.tasks.filter(task => task.id !== id)})),
-  on(loadTasks, state => ({...state}))
+  on(deleteTask, (state, { id }) => ({ ...state, tasks: state.tasks.filter(task => task.id !== id)}))
 );
-
